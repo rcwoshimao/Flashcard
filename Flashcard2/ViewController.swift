929df8d78b8a1ce934dad42d60365f385e7f066d
rcwoshimao <mantoumiaoshen@gmail.com>
2022-09-27 20:30:37 -0700

    Initial Commit

---
+++ Flashcard2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flashcard2
+//
+//  Created by Rebecca Chen on 2022/9/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
